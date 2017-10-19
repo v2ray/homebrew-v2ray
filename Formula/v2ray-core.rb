69235241654fcdc8f593ec529cce00ec5848f382
Doublemine <Doublemine.w@gmail.com>
2017-10-19 21:36:26 +0800

    initial commit

---
+++ Formula/v2ray-core.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class V2rayCore < Formula
+  desc "A platform for building proxies to bypass network restrictions."
+  homepage "https://www.v2ray.com/"
+  url "https://github.com/v2ray/v2ray-core/releases/download/v2.41/v2ray-macos.zip"
+  sha256 "0c49b05787bce5a8a181b4d2df2df1a4617c7d617165f6fbff2657fba0bc8439"
+
+  # depends_on "cmake" => :build
+
+  def install
+
+    bin.install "v2ray"
+
+    etc.install "config.json"
+
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test v2ray-core`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/v2ray", "-version"
+  end
+end
