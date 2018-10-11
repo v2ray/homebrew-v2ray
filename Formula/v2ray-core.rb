9f5ee54969c7b6a4e682f030e6a80e1365f2de1b
Darien Raymond <admin@v2ray.com>
2018-10-11 17:33:53 +0000

    update to version 3.47

--- Formula/v2ray-core.rb
+++ Formula/v2ray-core.rb
@@ -5,9 +5,9 @@
 class V2rayCore < Formula
   desc "A platform for building proxies to bypass network restrictions."
   homepage "https://www.v2ray.com/"
-  url "https://github.com/v2ray/v2ray-core/releases/download/v3.46/v2ray-macos.zip"
-  version "3.46"
-  sha256 "413daf989dc8fa1789c6f65f635fba9f360713c8ef1be0f268c07f57ee128929"
+  url "https://github.com/v2ray/v2ray-core/releases/download/v3.47/v2ray-macos.zip"
+  version "3.47"
+  sha256 "bd594f1068d7b5d9643f4c1ddec7bd129540bfc64ed9fefc90d0feec018f20c8"
 
   # depends_on "cmake" => :build
 
