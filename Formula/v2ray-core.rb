f92f613e64d93e6ba1627ff816b501541856167c
Bot <robot@v2ray.org>
2020-03-25 17:43:46 +0000

    travis automated update version 4.23.1

--- Formula/v2ray-core.rb
+++ Formula/v2ray-core.rb
@@ -5,9 +5,9 @@
 class V2rayCore < Formula
   desc "A platform for building proxies to bypass network restrictions."
   homepage "https://www.v2ray.com/"
-  url "https://github.com/v2ray/v2ray-core/releases/download/v4.22.1/v2ray-macos.zip"
-  version "4.22.1"
-  sha256 "dc743916843e0b2035274e6ac8ac9f1f47ebdb42fee08ef2c5cc277d08d3afda"
+  url "https://github.com/v2ray/v2ray-core/releases/download/v4.23.1/v2ray-macos.zip"
+  version "4.23.1"
+  sha256 "f0a72e96914bb671afb49abc1d09ac4222fea7e5e4a9844f62b7ca6907fa0062"
 
 
 
